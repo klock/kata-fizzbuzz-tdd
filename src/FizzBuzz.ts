ed2acd7d75039d71889c5155b88fbaf76a64539f
Math <math.klock@gmail.com>
2019-07-04 22:40:14 +0200

    Handle computation of 4 (refactor)

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,20 +1,10 @@
 export class FizzBuzz {
 
     compute(input: number) {
-        if (input === 1) {
-            return '1';
-        }
-
-        if (input === 2) {
-            return '2';
-        }
-
         if (input === 3) {
             return 'Fizz';
-        }
-
-        if (input === 4) {
-            return '4';
+        } else {
+            return input.toString();
         }
     }
 }
