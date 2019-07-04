dbcc8c103a671536179d18a88eaaf4bb8486056a
Math <math.klock@gmail.com>
2019-07-04 23:12:36 +0200

    Handle computation of 16 to 36

--- tests/FizzBuzz.test.ts
+++ tests/FizzBuzz.test.ts
@@ -75,4 +75,109 @@ describe('FizzBuzz', () => {
         const result = FizzBuzz.compute(15);
         expect(result).toBe('FizzBuzz');
     });
+
+    it('compute 16', () => {
+        const result = FizzBuzz.compute(16);
+        expect(result).toBe('16');
+    });
+
+    it('compute 17', () => {
+        const result = FizzBuzz.compute(17);
+        expect(result).toBe('17');
+    });
+
+    it('compute 18', () => {
+        const result = FizzBuzz.compute(18);
+        expect(result).toBe('Fizz');
+    });
+
+    it('compute 19', () => {
+        const result = FizzBuzz.compute(19);
+        expect(result).toBe('19');
+    });
+
+    it('compute 20', () => {
+        const result = FizzBuzz.compute(20);
+        expect(result).toBe('Buzz');
+    });
+
+    it('compute 21', () => {
+        const result = FizzBuzz.compute(21);
+        expect(result).toBe('Fizz');
+    });
+
+    it('compute 22', () => {
+        const result = FizzBuzz.compute(22);
+        expect(result).toBe('22');
+    });
+
+    it('compute 23', () => {
+        const result = FizzBuzz.compute(23);
+        expect(result).toBe('23');
+    });
+
+    it('compute 24', () => {
+        const result = FizzBuzz.compute(24);
+        expect(result).toBe('Fizz');
+    });
+
+    it('compute 25', () => {
+        const result = FizzBuzz.compute(25);
+        expect(result).toBe('Buzz');
+    });
+
+    it('compute 26', () => {
+        const result = FizzBuzz.compute(26);
+        expect(result).toBe('26');
+    });
+
+    it('compute 27', () => {
+        const result = FizzBuzz.compute(27);
+        expect(result).toBe('Fizz');
+    });
+
+    it('compute 28', () => {
+        const result = FizzBuzz.compute(28);
+        expect(result).toBe('28');
+    });
+
+    it('compute 29', () => {
+        const result = FizzBuzz.compute(29);
+        expect(result).toBe('29');
+    });
+
+    it('compute 30', () => {
+        const result = FizzBuzz.compute(30);
+        expect(result).toBe('FizzBuzz');
+    });
+
+    it('compute 31', () => {
+        const result = FizzBuzz.compute(31);
+        expect(result).toBe('31');
+    });
+
+    it('compute 32', () => {
+        const result = FizzBuzz.compute(32);
+        expect(result).toBe('32');
+    });
+
+    it('compute 33', () => {
+        const result = FizzBuzz.compute(33);
+        expect(result).toBe('Fizz');
+    });
+
+    it('compute 34', () => {
+        const result = FizzBuzz.compute(34);
+        expect(result).toBe('34');
+    });
+
+    it('compute 35', () => {
+        const result = FizzBuzz.compute(35);
+        expect(result).toBe('Buzz');
+    });
+
+    it('compute 36', () => {
+        const result = FizzBuzz.compute(36);
+        expect(result).toBe('Fizz');
+    });
 });
