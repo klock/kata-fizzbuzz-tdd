73ced487a9d1a781a8782552fbf7e2f08467ad0a
Math Klock <math.klock@gmail.com>
2019-07-05 09:31:38 +0200

    Refactor tests

--- tests/FizzBuzz.test.ts
+++ tests/FizzBuzz.test.ts
@@ -1,183 +1,41 @@
 import { FizzBuzz } from '../src/FizzBuzz';
 
+function expectResultToBeEqualToInput(input: number) {
+    expect(FizzBuzz.compute(input)).toBe(input.toString());
+}
+
+function expectResultToBe(input: number, expected: string) {
+    expect(FizzBuzz.compute(input)).toBe(expected);
+}
+
 describe('FizzBuzz', () => {
-    it('compute 1', () => {
-        const result = FizzBuzz.compute(1);
-        expect(result).toBe('1');
-    });
 
-    it('compute 2', () => {
-        const result = FizzBuzz.compute(2);
-        expect(result).toBe('2');
-    });
+    const equivalentNumbers: number[] = [ 1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19, 22, 23, 26, 28, 29, 31, 32, 34];
+    const fizzNumbers: number[] = [ 3, 6, 9, 12, 18, 21, 24, 27, 33, 36 ];
+    const buzzNumbers: number[] = [ 5, 10, 20, 25, 35 ];
+    const fizzBuzzNumbers: number[] = [ 15, 30 ];
 
-    it('compute 3', () => {
-        const result = FizzBuzz.compute(3);
-        expect(result).toBe('Fizz');
-    });
+    for (let input of equivalentNumbers) {
+        it('compute ' + input, () => {
+            expectResultToBeEqualToInput(input);
+        });
+    }
 
-    it('compute 4', () => {
-        const result = FizzBuzz.compute(4);
-        expect(result).toBe('4');
-    });
+    for (let input of fizzNumbers) {
+        it('compute ' + input, () => {
+            expectResultToBe(input, 'Fizz');
+        });
+    }
 
-    it('compute 5', () => {
-        const result = FizzBuzz.compute(5);
-        expect(result).toBe('Buzz');
-    });
+    for (let input of buzzNumbers) {
+        it('compute ' + input, () => {
+            expectResultToBe(input, 'Buzz');
+        });
+    }
 
-    it('compute 6', () => {
-        const result = FizzBuzz.compute(6);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 7', () => {
-        const result = FizzBuzz.compute(7);
-        expect(result).toBe('7');
-    });
-
-    it('compute 8', () => {
-        const result = FizzBuzz.compute(8);
-        expect(result).toBe('8');
-    });
-
-    it('compute 9', () => {
-        const result = FizzBuzz.compute(9);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 10', () => {
-        const result = FizzBuzz.compute(10);
-        expect(result).toBe('Buzz');
-    });
-
-    it('compute 11', () => {
-        const result = FizzBuzz.compute(11);
-        expect(result).toBe('11');
-    });
-
-    it('compute 12', () => {
-        const result = FizzBuzz.compute(12);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 13', () => {
-        const result = FizzBuzz.compute(13);
-        expect(result).toBe('13');
-    });
-
-    it('compute 14', () => {
-        const result = FizzBuzz.compute(14);
-        expect(result).toBe('14');
-    });
-
-    it('compute 15', () => {
-        const result = FizzBuzz.compute(15);
-        expect(result).toBe('FizzBuzz');
-    });
-
-    it('compute 16', () => {
-        const result = FizzBuzz.compute(16);
-        expect(result).toBe('16');
-    });
-
-    it('compute 17', () => {
-        const result = FizzBuzz.compute(17);
-        expect(result).toBe('17');
-    });
-
-    it('compute 18', () => {
-        const result = FizzBuzz.compute(18);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 19', () => {
-        const result = FizzBuzz.compute(19);
-        expect(result).toBe('19');
-    });
-
-    it('compute 20', () => {
-        const result = FizzBuzz.compute(20);
-        expect(result).toBe('Buzz');
-    });
-
-    it('compute 21', () => {
-        const result = FizzBuzz.compute(21);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 22', () => {
-        const result = FizzBuzz.compute(22);
-        expect(result).toBe('22');
-    });
-
-    it('compute 23', () => {
-        const result = FizzBuzz.compute(23);
-        expect(result).toBe('23');
-    });
-
-    it('compute 24', () => {
-        const result = FizzBuzz.compute(24);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 25', () => {
-        const result = FizzBuzz.compute(25);
-        expect(result).toBe('Buzz');
-    });
-
-    it('compute 26', () => {
-        const result = FizzBuzz.compute(26);
-        expect(result).toBe('26');
-    });
-
-    it('compute 27', () => {
-        const result = FizzBuzz.compute(27);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 28', () => {
-        const result = FizzBuzz.compute(28);
-        expect(result).toBe('28');
-    });
-
-    it('compute 29', () => {
-        const result = FizzBuzz.compute(29);
-        expect(result).toBe('29');
-    });
-
-    it('compute 30', () => {
-        const result = FizzBuzz.compute(30);
-        expect(result).toBe('FizzBuzz');
-    });
-
-    it('compute 31', () => {
-        const result = FizzBuzz.compute(31);
-        expect(result).toBe('31');
-    });
-
-    it('compute 32', () => {
-        const result = FizzBuzz.compute(32);
-        expect(result).toBe('32');
-    });
-
-    it('compute 33', () => {
-        const result = FizzBuzz.compute(33);
-        expect(result).toBe('Fizz');
-    });
-
-    it('compute 34', () => {
-        const result = FizzBuzz.compute(34);
-        expect(result).toBe('34');
-    });
-
-    it('compute 35', () => {
-        const result = FizzBuzz.compute(35);
-        expect(result).toBe('Buzz');
-    });
-
-    it('compute 36', () => {
-        const result = FizzBuzz.compute(36);
-        expect(result).toBe('Fizz');
-    });
+    for (let input of fizzBuzzNumbers) {
+        it('compute ' + input, () => {
+            expectResultToBe(input, 'FizzBuzz');
+        });
+    }
 });
